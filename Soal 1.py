b4e6b648492c763ab9b42575bd33a92afa9b5c0d
Rich Hutabarat <93704014+Richhtb@users.noreply.github.com>
2023-01-05 17:14:00 +0700

    Add files via upload

---
+++ Soal 1.py
@@ -0,0 +1,47 @@
+class NodePelanggan:
+    def __init__(self, namaPelanggan):
+        self._namaPelanggan = namaPelanggan
+    def getNamaPelanggan(self):
+        return self._namaPelanggan
+class WarungMakan:
+    DEFAULT_CAPACITY = 5
+    def __init__(self): #tidak boleh mengganti / menambah metode init
+        self._data = [None] * WarungMakan.DEFAULT_CAPACITY
+        self._size = 0
+        self._front = 0
+    def __len__(self):
+        return self._size
+    def is_empty(self):
+        return self._size == 0
+    def dequeue(self): #menghapus data paling depan, dan memajukan urutan data yang dibelangkangnya
+        self.dequee
+        pass
+    def enqueue(self, namaPelanggan): #menambah data ke list
+        self._data.append(namaPelanggan)
+        pass
+    def resizeBy3(self): #menambah ukuran queue sebesar 3
+        pass
+    def printAll(self):
+        print("\n=== WarungMakan ===")
+        for i in range(len(self._data)):
+            if self._data[i] != None:
+                print(i+1,end=". ")
+                print(self._data[i].getNamaPelanggan())
+            else:
+                print(i+1,end=". ")
+                print("Kosong")
+# test case program
+wm = WarungMakan()
+wm.enqueue("Pelanggan A")
+wm.enqueue("Pelanggan B")
+wm.enqueue("Pelanggan C")
+wm.enqueue("Pelanggan D")
+wm.enqueue("Pelanggan E")
+wm.printAll()
+wm.enqueue("Pelanggan F")
+wm.enqueue("Pelanggan G")
+wm.printAll()
+wm.dequeue()
+wm.dequeue()
+wm.dequeue()
+wm.printAll()
